656f0779e5ebda6b592cf7efb30ace033acb8bb2
Pathakorn Suksaeng <6210612773@cn103>
2021-10-31 13:49:38 +0700

    31-10-2021-02

--- project/about/tests.py
+++ project/about/tests.py
@@ -1,3 +1,57 @@
-from django.test import TestCase
+from django.http import request
+from django.shortcuts import get_object_or_404
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+
 
 # Create your tests here.
+
+
+
+
+class AboutModelTestCase(TestCase):
+
+    def test_index_about(self):
+        c = Client()
+        response = c.get(reverse('about:index'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_ER_about(self):
+        c = Client()
+        response = c.get(reverse('about:ER'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_Goals_about(self):
+        c = Client()
+        response = c.get(reverse('about:Goals'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_Personas_about(self):
+        c = Client()
+        response = c.get(reverse('about:Personas'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_stories_about(self):
+        c = Client()
+        response = c.get(reverse('about:stories'))
+        self.assertEqual(response.status_code, 200)
+    def test_Sitemap_about(self):
+        c = Client()
+        response = c.get(reverse('about:Sitemap'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_Descriptions_about(self):
+        c = Client()
+        response = c.get(reverse('about:Descriptions'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_Wireframes_about(self):
+        c = Client()
+        response = c.get(reverse('about:Wireframes'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_non_functional_about(self):
+        c = Client()
+        response = c.get(reverse('about:non_funtional'))
+        self.assertEqual(response.status_code, 200)
--- project/recipe/tests.py
+++ project/recipe/tests.py
@@ -1,3 +1,64 @@
-from django.test import TestCase
+from django.http import request
+from django.shortcuts import get_object_or_404
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from recipe.models import ingredient , recipe
+
 
 # Create your tests here.
+
+class RecipeModelTestCase(TestCase):
+
+    def test_index_recipe(self):
+        c = Client()
+        response = c.get(reverse('recipe:index'))
+        self.assertEqual(response.status_code, 200)
+
+    
+    def test_meat_recipe(self):
+        c = Client()
+        response = c.get(reverse('recipe:meat'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_norm_recipe(self):
+        c = Client()
+        response = c.get(reverse('recipe:norm'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_hal_recipe(self):
+        c = Client()
+        response = c.get(reverse('recipe:hal'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_bev_recipe(self):
+        c = Client()
+        response = c.get(reverse('recipe:bev'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_oco_recipe(self):
+        c = Client()
+        response = c.get(reverse('recipe:oco'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_vegy_recipe(self):
+        c = Client()
+        response = c.get(reverse('recipe:vegy'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_app_recipe(self):
+        c = Client()
+        response = c.get(reverse('recipe:app'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_mode_recipe(self):
+        c = Client()
+        response = c.get(reverse('recipe:mode'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_menu_recipe(self):
+        c = Client()
+        recipe1 = recipe.objects.create(menu='soup') 
+        response = c.get(reverse('recipe:menu', args=(recipe1.id,)))
+        self.assertEqual(response.status_code, 200)
+
